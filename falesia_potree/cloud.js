e656b12b79a0456bb8bc43e91b2ff8df80972ac2
GEOCE <Maynara19!>
2024-10-03 12:49:29 -0300

    etapa3

---
+++ falesia_potree/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=24 +south +ellps=GRS80 +units=m",
+    "points": 93506626,
+    "boundingBox": {
+        "lx": 598319.691604,
+        "ly": 9538278.055298,
+        "lz": -311.894910,
+        "ux": 601940.105857,
+        "uy": 9541898.469551,
+        "uz": 3308.519343
+    },
+    "tightBoundingBox": {
+        "lx": 598652.207495,
+        "ly": 9538640.671189,
+        "lz": -30.632290,
+        "ux": 601086.969023,
+        "uy": 9540906.640286,
+        "uz": 76.285655
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 28.284486,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
